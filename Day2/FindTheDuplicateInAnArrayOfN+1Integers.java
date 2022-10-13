15c71e4e4fe1e4ee29dabfeb2e71d6404c56e173
agentgrey <himadrinayak2702@gmail.com>
2022-10-14 00:12:51 +0530

    So many methods

---
+++ Day2/FindTheDuplicateInAnArrayOfN+1Integers.java
@@ -0,0 +1,125 @@
+// Make a frequency array
+// Time:O(N)  Space:O(N)
+
+
+
+
+public static int findDuplicate(int[] nums) {
+        int len = nums.length;
+        int[] cnt = new int[len + 1];
+        for (int i = 0; i < len; i++) {
+            cnt[nums[i]]++;
+            if (cnt[nums[i]] > 1) {
+                return nums[i];
+            }
+        }
+
+        return len;
+    }
+
+
+
+
+
+// Make a set and return if the element is already stored in it
+// Time:O(N)  Space:O(N)
+
+
+
+
+public static int findDuplicate_set(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        int len = nums.length;
+        for (int i = 0; i < len; i++) {
+            if (!set.add(nums[i])) {
+                return nums[i];
+            }
+        }
+
+        return len;
+    }
+
+
+
+
+// Mark visited value within the array
+// while traversing from left to right set the nums[n] to its -ve value
+// Time:O(N)  Space:O(1)
+
+
+
+
+public static int findDuplicate_mark(int[] nums) {
+        int len = nums.length;
+        for (int num : nums) {
+            int idx = Math.abs(num);
+            if (nums[idx] < 0) {
+                return idx;
+            }
+            nums[idx] = -nums[idx];
+        }
+
+        return len;
+    }
+
+
+
+
+// Binary Search
+// If cnt is greater than mid, repeated elements are in interval [left, mid]
+// Else the repeated element is in the interval [mid+1, right]
+// Time:O(NlogN)  Space:O(1)
+
+
+
+
+
+public static int findDuplicate_bs(int[] nums) {
+        int len = nums.length;
+        int low = 1;
+        int high = len - 1;
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+            int cnt = 0;
+            for (int i = 0; i < len; i++) {
+                if (nums[i] <= mid) {
+                    cnt++;
+                }
+            }
+
+            if (cnt <= mid) {
+                low = mid + 1;
+            } else {
+                high = mid;
+            }
+        }
+
+        return low;
+    }
+
+
+
+
+// Assume it to be a Linked List
+// Repeated element is present where it forms a cycle
+// Time:O(N)  Space:O(1)
+
+
+
+
+public int findDuplicate_fastSlow(int[] nums) {
+        int slow = 0;
+        int fast = 0;
+        do {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        } while (slow != fast);
+
+        slow = 0;
+        while (slow != fast) {
+            slow = nums[slow];
+            fast = nums[fast];
+        }
+        
+        return slow;
+    }
