350d2b61c306b63fbac791b0210143fa1af69d66
agentgrey <himadrinayak2702@gmail.com>
2022-10-17 16:07:55 +0530

    Arrays4

---
+++ Day4/LargestSubarrayWith0Sum.java
@@ -0,0 +1,35 @@
+// Using HashSet
+// let’s say we know that the sum of subarray(i, j) = S, and we also know that sum of subarray(i, x) = S where i < x < j. We can conclude that the sum of subarray(x+1, j) = 0
+// Time:O(N)  Space:O(N)
+
+
+
+
+int maxLen(int A[], int n)
+    {
+
+        HashMap<Integer, Integer> mpp = new HashMap<Integer, Integer>();
+
+        int maxi = 0;
+        int sum = 0; 
+
+        for(int i = 0;i<n;i++) {
+
+            sum += A[i]; 
+
+            if(sum == 0) {
+                maxi = i + 1; 
+            }
+            else {
+                if(mpp.get(sum) != null) {
+
+                    maxi = Math.max(maxi, i - mpp.get(sum)); 
+                }
+                else {
+
+                    mpp.put(sum, i); 
+                }
+            }
+        }
+        return maxi; 
+    }
