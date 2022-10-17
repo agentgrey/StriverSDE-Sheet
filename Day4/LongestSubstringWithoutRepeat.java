aa868678fee3ed396236667819e0d37a666cfffd
agentgrey <himadrinayak2702@gmail.com>
2022-10-18 00:47:23 +0530

    Arrays4:

---
+++ Day4/LongestSubstringWithoutRepeat.java
@@ -0,0 +1,56 @@
+// Using HashMap and left and right pointer
+// Pointer ‘left’ is used for maintaining the starting point of substring while ‘right’ will maintain the endpoint of the substring.
+// 'right' pointer will move forward and check for the duplicate occurrence of the current element if found then ‘left’ pointer will be shifted ahead so as to delete the duplicate elements.
+// Time:O(2*N)  Space:O(N)
+
+
+
+
+static int solve(String str) {
+
+        if(str.length()==0)
+             return 0;
+        int maxans = Integer.MIN_VALUE;
+        Set < Character > set = new HashSet < > ();
+        int l = 0;
+        for (int r = 0; r < str.length(); r++) // outer loop for traversing the string
+        {
+            if (set.contains(str.charAt(r))) //if duplicate element is found
+            {
+                while (l < r && set.contains(str.charAt(r))) {
+                    set.remove(str.charAt(l));
+                    l++;
+                }
+            }
+            set.add(str.charAt(r));
+            maxans = Math.max(maxans, r - l + 1);
+        }
+        return maxans;
+    }
+
+
+
+
+
+// make a map that will take care of counting the elements and maintaining the frequency of each and every element as a unity by taking the latest index of every element
+// Time:O(N)  Space:O(N)
+
+
+
+
+static int solve(String s) {
+        HashMap < Character, Integer > mpp = new HashMap < Character, Integer > ();
+
+        int left = 0, right = 0;
+        int n = s.length();
+        int len = 0;
+        while (right < n) {
+            if (mpp.containsKey(s.charAt(right))) left = Math.max(mpp.get(s.charAt(right)) + 1, left);
+
+            mpp.put(s.charAt(right), right);
+
+            len = Math.max(len, right - left + 1);
+            right++;
+        }
+        return len;
+    }
