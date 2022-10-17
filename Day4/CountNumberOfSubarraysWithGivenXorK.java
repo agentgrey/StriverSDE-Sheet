1a1945d1d65920dfc4c9231031b02faf51191579
agentgrey <himadrinayak2702@gmail.com>
2022-10-17 23:57:32 +0530

    Arrays4

---
+++ Day4/CountNumberOfSubarraysWithGivenXorK.java
@@ -0,0 +1,27 @@
+// Usinf Prefix XOR and Map
+// let p>q, then xor[0 to p]^xor[0 to q] = xor[q to p]
+// Time:O(N)  Space:O(N) 
+
+
+
+
+
+public int solve(int[] A, int B) {
+     HashMap<Integer,Integer> visited = new HashMap<Integer,Integer>(); 
+        int c = 0; 
+        int cpx = 0;
+        int n = A.length;
+        for(int i = 0;i<n;i++) {
+            cpx = cpx ^ A[i]; 
+            if(visited.get(cpx^B) != null) 
+                c += visited.get(cpx ^ B); 
+            if(cpx == B) {
+                c++; 
+            }
+            if(visited.get(cpx) != null) 
+                visited.put(cpx, visited.get(cpx) + 1); 
+            else 
+                visited.put(cpx, 1); 
+        }
+        return c; 
+    }
