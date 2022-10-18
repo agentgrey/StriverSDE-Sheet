f2fe88d7c71128f0757c1f98ad8a3a63aa9d5a92
agentgrey <himadrinayak2702@gmail.com>
2022-10-18 15:52:32 +0530

    LinkedList

---
+++ Day5/RemoveNthNodeFromBackOfLinkedList.java
@@ -0,0 +1,22 @@
+// Two pointer
+// Time:O(N)  Space:O(1)
+
+
+
+
+public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode newHead = new ListNode(0);
+        newHead.next = head;
+        ListNode a = newHead;
+        ListNode b = newHead;
+        while(n > 0){
+            b = b.next;
+            n--;
+        }
+        while(b.next != null){
+            b = b.next;
+            a = a.next;
+        }
+        a.next = a.next.next;
+        return newHead.next;
+    }
